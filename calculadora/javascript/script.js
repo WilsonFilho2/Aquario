f4704e579b673e3017d9a556db9a283261f43027
WilsonFilho <wilson.menezes09@aluno.ifce.edu.br>
2022-11-09 13:05:26 -0300

    Calculadora

--- calculadora/javascript/script.js
+++ calculadora/javascript/script.js
@@ -14,13 +14,53 @@ button.addEventListener("click", calcular);
 //Criando Função
 
 function calcular () {
-    //Volume
+    if (parseFloat(altura.value.length) != 0 && parseFloat(comprimento.value.length) != 0 && parseFloat(largura.value.length) != 0){ //Se nem todos os inputs receberem valores, o site trava :(
+        //Volume
+        let altura_v = parseFloat(altura.value);
+        let largura_v = parseFloat(largura.value);
+        let comprimento_v = parseFloat(comprimento.value);
+        let volume_v =  altura_v * largura_v * comprimento_v / 1000
+        volume.innerHTML = `Volume: ${volume_v}L`;
 
-    let altura_v = parseFloat(altura.value);
-    let largura_v = parseFloat(largura.value);
-    let comprimento_v = parseFloat(comprimento.value);
-    volume.innerHTML = `Volume: ${altura_v * largura_v * comprimento_v / 1000}L`;
 
+        //Aquecedor
+        let i = 0;
+        let a = 50;
+    
+        while (true){
+            i += 1;
+            if (a*i >= volume_v){
+                aquecedor.innerHTML = `Aquecedor: ${a*i}W`;
+                break;
+            }
+        }
+        //Bomba
 
-    //Aquecedor
+        litro_hora.innerHTML = `Litro/Hora: ${(volume_v / 6).toFixed(2)}L/H`;
+
+        altura.style.borderBottom = "1px solid #060a18";
+        comprimento.style.borderBottom = "1px solid #060a18";
+        largura.style.borderBottom = "1px solid #060a18";
+
+    }
+    else {
+        if (parseFloat(altura.value.length) == 0){
+            alert("Está Faltando a altura do aquário");
+            altura.style.borderBottom = "1px solid red";
+            comprimento.style.borderBottom = "1px solid #060a18";
+            largura.style.borderBottom = "1px solid #060a18";
+        }
+        else if (parseFloat(largura.value.length) == 0){
+            alert("Está faltando a largura do aquário");
+            largura.style.borderBottom = "1px solid red";
+            comprimento.style.borderBottom = "1px solid #060a18";
+            altura.style.borderBottom = "1px solid #060a18";
+        }
+        else if (parseFloat(comprimento.value.length) == 0){
+            alert("Está faltando o comprimento do aquário");
+            comprimento.style.borderBottom = "1px solid red";
+            altura.style.borderBottom = "1px solid #060a18";
+            largura.style.borderBottom = "1px solid #060a18";
+        }
+    }
 }
